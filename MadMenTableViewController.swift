5b6e1fb795bc2f96696b91253d32a018fdc40c5f
Erica Millado <ericamillado@gmail.com>
2017-06-13 20:12:20 -0400

    Implemented Core Data

--- MadMenTableViewController.swift
+++ MadMenTableViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import CoreData
 
 class MadMenTableViewController: UITableViewController {
     
-    var madMenCharacters: [String] = ["Joan Holloway", "Don Draper", "Roger Sterling", "Bertram Cooper", "Pete Campbell", "Peggy Olson"]
+//    var madMenCharacters: [String] = ["Joan Holloway", "Don Draper", "Roger Sterling", "Bertram Cooper", "Pete Campbell", "Peggy Olson"]
+    var characters: [NSManagedObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        fetchData()
+    }
+    
+    // MARK: - Core Data Fetch and Save
+    
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Character", in: managedContext)!
+        let character = NSManagedObject(entity: entity, insertInto: managedContext)
+        character.setValue(name, forKey: "name")
+        
+        do {
+            try managedContext.save()
+            characters.append(character)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    //FETCH FROM CORE DATA
+    func fetchData() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Character")
+        do {
+            characters = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
 
     // MARK: - Navigation
     
@@ -22,7 +58,9 @@ class MadMenTableViewController: UITableViewController {
         
         if sender.source is AddCharacterViewController {
             if let senderVC = sender.source as? AddCharacterViewController {
-                madMenCharacters.append(senderVC.character)
+                //SAVE TO CORE DATA
+                save(name: senderVC.character)
+//                madMenCharacters.append(senderVC.character)
             }
             tableView.reloadData()
         }
@@ -35,12 +73,14 @@ class MadMenTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return madMenCharacters.count
+//        return madMenCharacters.count
+        return characters.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "madMenCell", for: indexPath)
-        cell.textLabel?.text = madMenCharacters[indexPath.row]
+//        cell.textLabel?.text = madMenCharacters[indexPath.row]
+        cell.textLabel?.text = characters[indexPath.row].value(forKey: "name") as? String
         cell.imageView?.image = UIImage(named: "madMen")
         return cell
     }
